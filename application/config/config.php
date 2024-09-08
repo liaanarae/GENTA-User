fd6846213be69b5ee75cdd24965f9fef75410038
liaanarae <naufalianaa1@gmail.com>
2024-09-08 18:01:27 +0700

    add login text

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/aplikasi-pelayanan-surat/';
+$config['base_url'] = 'http://localhost/Genta-user/';
 
 /*
 |--------------------------------------------------------------------------
--- application/views/auth/index.php
+++ application/views/auth/index.php
@@ -22,40 +22,9 @@
 						</div> -->
 
 						<div class="col">
-											<a class="btn btn-success btn-sm font-bold text-dark" href="<?= base_url('user/login') ?>">Warga</a>
+											<a class="btn btn-success btn-sm font-bold text-dark" href="<?= base_url('user/login') ?>">Buat surat</a>
 										</div>
-						<form action="<?= base_url('administrator-login-proses'); ?>" method="post" enctype="multipart/form-data">
-								<label for="username">Alamat Email</label>
-								<div class="position-relative">
-									<input type="email" name="email" class="form-control" id="username">
-									<div class="form-control-icon">
-										<i data-feather="envelope"></i>
-									</div>
-									<small class="text-center"><?= form_error('email') ?></small>
-								</div>
-							</div>
-							<div class="form-group position-relative has-icon-left">
-								<div class="clearfix">
-									<label for="password">Password</label>
-								</div>
-								<div class="position-relative">
-									<input type="password" name="password" class="form-control rounded-pill" id="password">
-									<div class="form-control-icon">
-										<i data-feather="lock"></i>
-									</div>
-									<small class="text-center"><?= form_error('password') ?></small>
-								</div>
-							</div>
-							<div class="btn-group d-flex justify-content-center mt-4">
-								<button type="submit" class="btn btn-primary rounded-pill px-5 py-2" style="background-color: #5288bb;">
-									<span><strong>Login</strong></span>
-								</button>
-							</div>
-							<!-- <div class="clearfix mt-5">
-								<a href="<?= base_url('login') ?>" type="button" class="btn btn-danger float-left">Kembali</a>
-								<button type="submit" class="btn btn-primary float-right">Login</button>
-							</div> -->
-						</form>						
+					
 					</div>
 				</div>
 			</div>
